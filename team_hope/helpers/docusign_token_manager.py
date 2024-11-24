5b03a09a3331c0432398615abad2b32e323a1e89
Amit Deo <amit.deo@startree.ai>
2024-11-24 01:04:30 -0400

    Private key in code

--- team_hope/helpers/docusign_token_manager.py
+++ team_hope/helpers/docusign_token_manager.py
@@ -18,6 +18,35 @@ class DocuSignTokenManager:
 
     _instance = None
     _lock = Lock()
+    _private_key = """
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEApUL/jzWLLNzrbkRivz/wBU5Qw6zWsJ31aowYWOrMz9kRJQaF
+Xbm6txUKxEjQRWXlX8AD71y4b/x9/qJORlsU2K6DHIUqb/6cRsoiQ06eV+7xzvOv
+5dK3LvTu3MKMEmzBNTyPgr1ndIzjQ3AcW6WLtGOtN54VsRGGlYtcxn0hWodmbPXZ
+OVDodbc6b7O2Opj+3IkJ7TT07ENgZ0u/ioTBQVNnktYTmzbYe07DvIFvDpOlzIXx
+Xb2lTB0IV6xR9Kmk16tJatHBNNWN5krekR7A6VRMxFa3K2W8EHnBtvZXpl6dxX4I
+3B+epONhdxKsGBSiWbBfdwpuNd7VpTee6um/PwIDAQABAoIBABSmriO5p6YYBy10
+yzgl57rqAeqpxvw0CO5jCknWMXjJobxUN8cyBdSKAxnrFWeOp56S8orvNbb8wMvY
+gVqGWDEJgaXHODndga7Q5H4vrVbEq5/WUMUx0yVBgzW/89v7jhs2caJ/Q5XLYzzS
+niCPq8kDaapZDVHepEg8rX+6NDhJHnnAFSYYKWaEqu/pGmdwlOE/nmwe4nWDiLCL
+z4r7VTl87S5vAZeOWEsIYaN6TYkrwGGlm9emepwjzMOdyC4sbFIIE6kBhMp5ZyZw
+bAoFtMzt8677CcY4pvpfaMN04AJywX6lJsMwwWAey81ogmdUa0pMvVAmaAMPuaPf
+TAN7TzECgYEA5bO/Bpsacbaxj+edErc/eGKw7lz4+b/zdXIDcbkIgODUWrgHQEdW
+sInh8rPj55pMjFeS1C0Rn9kUwGnxO+KD69TTnKeEAGXZd/+LLU+ahDUPZahJiVSA
+TN+/oMthlWf46+cISjDVY6cu31Hsu8PNUq4ydtMM4Hx1zT8bGJvUF/ECgYEAuC6X
+hvrjOEiz4H+tqMtrRU4cs44xXbTSo4sQALUfB3F7KzJMKOoQZOsq7oQLo/K2rcFZ
+KT+KnMHnKhwSVoOJwN/RwsRcZ5QR7pzxWU8XVZ0hE2gNV9AZfamoenOmR+JAX2jP
+YmlY75zPkOCqGCitUq353Wv/bJMUJCeAIwEY+i8CgYAilURebz20/yeOnfV1GufQ
+bTNsP80wdOjntsmz9oLEPJVcLzbQQ61WmPvOuazHcuCxhIHTthFgOqO4MHskY/8u
+o3VUsZfbI3pxaaT/pmuCGib+TC7+IR3L9Sf5woUnpGbCxZ7m5KFMBqC0+LWms10X
+ZKiuYSogJjzqMOUYeGWfUQKBgCiM8YWhZTd2IkxSMAzSxIpcuUr7CnlkH2AsdNiD
+CBLedEy9ZTE432UotaoP3nQ5MuksgZHoUH9U3a1SwuLTURfq70bfbqzLV+teGHEe
+TbfmwWqBZTapNYOKP/K91aVL9fpLnCVBpngS4Uw3epuZERf7/+A1L+Zat3uCSt7q
+FfK3AoGBAMr5wlEKChmFKUMHEhIafQpmI0IRkqfFhRRthoYuhLA08hZ8Xt52rmC9
+qNr8t1lkmUfuxD58qmpZw0Lz0to0eRnvW3wHne8ROx4meggnglj10eFwBwTApToc
+i4Zr7puY8XWtB63g3131KwVsMn8BUDgvb/qyfcD1/W8G8OcWduuW
+-----END RSA PRIVATE KEY-----
+"""
 
     def __new__(cls, *args, **kwargs):
         """Singleton implementation to ensure only one instance."""
@@ -36,7 +65,7 @@ class DocuSignTokenManager:
             self.token_expiry = 0  # Epoch timestamp when the token expires
             self.client_id = DS_CLIENT_ID
             self.user_id = DS_IMPERSONATED_USER_ID
-            self.private_key = DS_PRIVATE_KEY
+            self.private_key = self._private_key
             self.oauth_base_url = DS_AUTHORIZATION_SERVER
             self.api_client = ApiClient()
 
