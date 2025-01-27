71e4647f4c634905857414b8be75a0d3b9c17d00
Amit Deo <amit.deo@startree.ai>
2025-01-26 22:44:42 -0800

    Admin fields and filter

--- team_hope/admin.py
+++ team_hope/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import UserProfile, UserIdentityInfo
 
-admin.site.register(UserProfile)
 admin.site.register(UserIdentityInfo)
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "surgery_date", "surgery_type", "teamhope_member_role")
+    list_filter = ("surgery_date", "surgery_type", "teamhope_member_role")
