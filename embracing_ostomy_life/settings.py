e2653a483e182ec28e8821407913adf77fd416f0
Kalenshi Katebe <kalenshi@gmail.com>
2025-03-17 22:18:54 -0300

    fixed bug on profile picture upload/get

--- embracing_ostomy_life/settings.py
+++ embracing_ostomy_life/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DEBUG = os.environ.get("DEBUG", False)
 # Take environment variables from .env file
 environ.Env.read_env(BASE_DIR / '.env')
 DEBUG = os.environ.get("DEBUG", True)
