968f421f8c0b0cc008c2dfc3bb4076575c62730a
Kalenshi Katebe <kalenshi@gmail.com>
2025-07-22 13:57:25 -0300

    updated home page css and debug to false

--- embracing_ostomy_life/settings.py
+++ embracing_ostomy_life/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG = True
+DEBUG = False
 # Take environment variables from .env file
 environ.Env.read_env(BASE_DIR / '.env')
 
